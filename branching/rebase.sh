e7c72ddb7b2be0a91a8902d7b5e17ed1a11f882c
AlxUp <alexander.upkin@yandex.ru>
2023-01-31 12:30:52 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,24 +2,12 @@
 
 # display command line options
 
-
 count=1
 
 for param in "$@"; do
 
-<<<<<<< HEAD
-    echo "Next parameter: $param"
-=======
     echo "\$@ Parameter #$count = $param"
->>>>>>> 199fe2e (rebase.sh)
 
     count=$(( $count + 1 ))
 
 done
-
-
-echo "====="
-<<<<<<< HEAD
-
-=======
->>>>>>> 199fe2e (rebase.sh)
